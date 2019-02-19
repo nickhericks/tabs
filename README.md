2bee856c6a9a32f971625ca83ec2239dab01d034
Nick-Hericks <nickhericks@gmail.com>
2019-02-19 16:12:25 +0200

    Update CSS styles

--- README.md
+++ README.md
@@ -4,6 +4,18 @@
 ## View project
  :mag: Live version available at [nickhericks.github.io/tabs/](https://nickhericks.github.io/tabs/)
 
+## What I learned
+- More about dom traversal
+- The `:not()` pseudo-class selector. For example, using it to give styling to elements that do not have the .is-selected class applied to them.
+
+```JavaScript
+
+.tab:not(.is-selected) .tab__icon {
+  color: rgba(255, 255, 255, 0.25);
+}
+
+```
+
 ## Acknowledgements
 This project is part of a great JS course called [Learn JavaScript](https://learnjavascript.today/). Thank you to the course creator [@zellwk](https://github.com/zellwk) :raised_hands:
 
--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
-// START EDITING YOUR JAVASCRIPT HERE
-// ===============
+
+
+
+const component = document.querySelector('.tabbed-component');
+const tabs = Array.from(component.querySelectorAll('.tab'));
+const contents = Array.from(component.querySelectorAll('.tab-content'));
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', _ => {
+    const link = tab.children[0]
+    const href = link.getAttribute('href')
+    console.log(href)
+    const tabContent = component.querySelector(href)
+    tabs.forEach(elem => elem.classList.remove('is-selected'))
+    contents.forEach(elem => elem.classList.remove('is-selected'))
+    tab.classList.add('is-selected')
+    tabContent.classList.add('is-selected')
+
+
+  })
+})
