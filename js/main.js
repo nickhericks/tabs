00ddf9ba2df3f7bd9bada54c849c97827588e079
Nick-Hericks <nickhericks@gmail.com>
2019-03-12 12:54:56 +0200

    Refactor tabs

--- js/main.js
+++ js/main.js
@@ -3,20 +3,23 @@ const tabList = component.querySelector(".tabs");
 const tabs = [...component.querySelectorAll(".tab")];
 const contents = [...component.querySelectorAll(".tab-content")];
 
-tabList.addEventListener("click", e => {
-  // Prevent tab from jumping
-  e.preventDefault();
+const toggleTabs = (e, component) => {
+	const link = e.target.closest("a");
+	const tab = link.parentNode;
+	const href = link.getAttribute("href");
+	const tabContent = component.querySelector(href);
 
-  const link = e.target.closest("a");
-  const tab = link.parentNode;
-  const href = link.getAttribute("href");
-  const tabContent = component.querySelector(href);
+	// Hiding previous tab and tab content
+	tabs.forEach(elem => elem.classList.remove("is-selected"));
+	contents.forEach(elem => elem.classList.remove("is-selected"));
 
-  // Hiding previous tab and tab content
-  tabs.forEach(elem => elem.classList.remove("is-selected"));
-  contents.forEach(elem => elem.classList.remove("is-selected"));
+	// Showing the selected tab and tab content
+	tab.classList.add("is-selected");
+	tabContent.classList.add("is-selected");
+}
 
-  // Showing the selected tab and tab content
-  tab.classList.add("is-selected");
-  tabContent.classList.add("is-selected");
+tabList.addEventListener('click', e => {
+	// Prevent tab from jumping
+	e.preventDefault();
+	toggleTabs(e, component);
 });
